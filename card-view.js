62601d965f9ebf65c353d7134123a566d5df7409
Fernando290608 <fernandopena58@aragon.unam.mx>
2024-05-30 13:28:36 -0600

    Se agregó la petición a la API rick and morty

---
+++ card-view.js
@@ -0,0 +1,53 @@
+import { LitElement, html, css } from "lit-element";
+
+class CardView extends LitElement {
+  static get properties() {
+    return {
+      character: { type: Object },
+    };
+  }
+
+  static get styles() {
+    return css`
+      div {
+        width: 250px;
+        display: flex;
+        flex-direction: column;
+        height: auto;
+        border-radius: 8px;
+        padding: 16px;
+        justify-content: center;
+        align-items: center;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+        box-sizing: border-box;
+
+        img {
+          width: 100%;
+          height: 100%;
+          border-radius: 50%;
+        }
+        figure {
+          width: 75%;
+          height: auto;
+        }
+      }
+    `;
+  }
+  constructor() {
+    super();
+    this.character = {};
+  }
+
+  render() {
+    return html`
+      <div>
+        <figure>
+          <img src=${this.character.image} />
+        </figure>
+        <h3>${this.character.name}</h3>
+      </div>
+    `;
+  }
+}
+
+customElements.define("card-view", CardView);
--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-import { LitElement, html } from "lit-element";
-
+import { LitElement, html, css } from "lit-element";
+import "./card-view";
 class HomePage extends LitElement {
   static get styles() {
-    return []
+    return css`
+      div {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: flex-start;
+        flex-wrap: wrap;
+        box-sizing: border-box;
+        gap: 2rem;
+        overflow: auto;
+        padding: 1.5rem;
+      }
+
+      h1 {
+        width: 100%;
+        text-align: center;
+      }
+    `;
   }
 
   static get properties() {
-    return {}
+    return {
+      rickAndMortyData: { type: Array },
+    };
+  }
+  async getDataApi() {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    this.rickAndMortyData = data.results;
   }
 
   constructor() {
-    super()
+    super();
+    this.rickAndMortyData = [];
   }
 
   render() {
     return html`
-      <p>Home page</p>
-    `
+      <div>
+        <h1>Rick and Morty</h1>
+        ${this.rickAndMortyData.map(
+          (character) => html`<card-view .character=${character}></card-view>`
+        )}
+      </div>
+    `;
+  }
+
+  firstUpdated() {
+    this.getDataApi();
   }
 }
 
-customElements.define("home-page", HomePage)
+customElements.define("home-page", HomePage);
